835936f5e5b6947271e2c5a9bab783f6d2b145b3
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-11-27 23:54:51 +0600

    Showing States

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -10,7 +10,7 @@ import {Status} from "../enum/status.enum";
   providedIn: 'root'
 })
 export class ServerService {
-  private readonly apiUrl = "any";
+  private readonly apiUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) {
   }
